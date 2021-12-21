544371e47d51ecf5b9d4d9ac526349ff06856a85
Devansh Soni <devanshsoni811@gmail.com>
2021-12-21 17:10:53 +0530

    Update subscription.php

--- includes/subscription.php
+++ includes/subscription.php
@@ -1,4 +1,9 @@
 <?php
+
+
+    header("Access-Control-Allow-Origin: *");
+    header('Content-Type: application/json');
+
     $conn = mysqli_connect("127.0.0.1", "root" ,"", "ecell");
     if(mysqli_connect_errno()){
     
